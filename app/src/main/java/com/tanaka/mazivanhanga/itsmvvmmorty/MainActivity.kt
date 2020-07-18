9cc521f03f5d2d17e0f52ef43999f86f3da30a49
tanakaderoy <tanaka.deroy@gmail.com>
2020-07-18 01:11:29 -0400

    initial commit

---
+++ app/src/main/java/com/tanaka/mazivanhanga/itsmvvmmorty/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tanaka.mazivanhanga.itsmvvmmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
